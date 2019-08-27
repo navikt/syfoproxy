bb78d090c39a5787f53cf1960b3ab96605abc750
Martin Solheim <martin.solheim@me.com>
2019-08-27 16:29:51 +0200

    Legger ved idtoken i authorization header

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 source /var/run/secrets/nais.io/vault/credentials.env
-envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY'
 
 exec nginx -g 'daemon off;'
