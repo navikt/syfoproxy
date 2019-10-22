952b5856e4e3ba995dc4e3dd25989cf4a37a5a3d
Tia Firing <tia.firing@nav.no>
2019-10-22 14:01:20 +0200

    Forsøker å legge til metrikker

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@
 source /var/run/secrets/nais.io/vault/credentials.env
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
-exec nginx -g 'daemon off;'
+exec nginx -g 'daemon off;' --with-http_stub_status_module
